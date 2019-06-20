7747cb740d6ed85c1144724aa5ce736635e54fee
BestPlagueDoctor <sam@bigmama.localdomain>
2019-06-20 16:27:04 -0500

    Array fixed

--- array/revamp/calc.cpp
+++ array/revamp/calc.cpp
@@ -1,11 +1,11 @@
 #include "proto.h"
 #include <cmath>
-int findx(int xres, int i) {
-  int xcoord = floor(i%xres);
+float findx(int xres, int i) {
+  float xcoord = floor(i%xres);
   return xcoord;
 }
-int findy(int xres, int i) {
-  int ycoord = floor(i/xres);
+float findy(int xres, int i) {
+  float ycoord = floor(i/xres);
   return ycoord;
 }
 
@@ -43,19 +43,22 @@ void imgmandel(int maxiter, int *img, int res, int xres, int yres) {
   complexnumber z;
   complexnumber zsq;
   for (i = 0; i < res; i++) {
-    int iters = 0;
-    float rec = findx(xres, i), imc = findy(xres, i);
+    float rec = findx(xres, i);
+    float imc = findy(xres, i);
     z.setcomp(0.0, 0.0); //z=(0+0i)
-    zsq.setcomp(0.0, 0.0); //zsq=(0+0i)
-    c.setcomp(rec, imc); //c=(r+i)
-    for (iters = 0; iters < maxiter; iters++) {
-      zsq.multcomp(z, z); //zsq=z*z
-      z.addcomp(zsq, c); //z=zsq+c
-      if (z.sqmagnit() >= 4) {
+    zsq.setcomp(0.0, 0.0);
+    float nrec = (((rec*3)/xres)-2), nimc = (((imc*2.4)/yres)-1.2);
+    //printf("(%.2f  %.2f) ", nrec, nimc);
+    c.setcomp(nrec, nimc);
+    int iters = 0;
+    for (iters = 0; iters < 4096; iters++) {
+      z.multcomp(z, z); //zsq=z*z
+      z.addcomp(z, c); //z=zsq+c
+      if (z.sqmagnit() >= 4.0) {
         break;
       }
     }
+    //printf("%d", iters);
     img[i] = iters;  
-    printf("%d \n", img[i]);
   }
 }
--- multi/calc.cpp
+++ multi/calc.cpp
@@ -7,9 +7,11 @@ int pointcheck(int maxiter, float rec, float imc) {
   zsq.setcomp(0.0, 0.0); //zsq=(0+0i)
   c.setcomp(rec, imc); //c=(r+i)
   int i;
-  for (i = 0; i < maxiter; i++) {
+  for (i = 0; i < 2000; i++) {
     zsq.multcomp(z, z); //zsq=z*z
+    //printf("(%.2f %.2f)   ",zsq.getreal(), zsq.getimag()); 
     z.addcomp(zsq, c); //z=zsq+c
+    //printf("  (%.2f %.2f) ",z.getreal(), z.getimag()); 
     if (z.sqmagnit() >= 4) {break;}
   }
   return i;
