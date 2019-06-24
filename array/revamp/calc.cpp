4ac14a29a42d23c25c02786d6b1ab970582a98ea
BestPlagueDoctor <sam@bigmama.localdomain>
2019-06-24 15:07:06 -0500

    Hi there

--- array/revamp/calc.cpp
+++ array/revamp/calc.cpp
@@ -37,21 +37,26 @@ void printcoord(int *img, int xres, int yres, int res) {
 }
 
 void imgmandel(int maxiter, int *img, int res, int xres, int yres) {
-  setvals(img, xres, yres);
   int i = 0;
+  int newit = 0;
+  setvals(img, xres, yres);
   complexnumber c;
   complexnumber z;
   complexnumber zsq;
   for (i = 0; i < res; i++) {
+    printf("I am iterating the loop for the %dth time \n", i);
     float rec = findx(xres, i);
     float imc = findy(xres, i);
     z.setcomp(0.0, 0.0); //z=(0+0i)
     zsq.setcomp(0.0, 0.0);
     float nrec = (((rec*3)/xres)-2), nimc = (((imc*2.4)/yres)-1.2);
-    //printf("(%.2f  %.2f) ", nrec, nimc);
     c.setcomp(nrec, nimc);
     int iters = 0;
     for (iters = 0; iters < 4096; iters++) {
+      //if (iters == 4095) {
+        //newit+=1;
+        //printf("I hit the ceiling for the %dth time\n", newit);
+      //}
       z.multcomp(z, z); //zsq=z*z
       z.addcomp(z, c); //z=zsq+c
       if (z.sqmagnit() >= 4.0) {
