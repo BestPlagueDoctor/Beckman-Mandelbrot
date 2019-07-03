7feb2d4a150daba031db7c0102084dcfc1c66183
BestPlagueDoctor <ksam1337@gmail.com>
2019-07-02 21:44:34 -0500

    Changed to cmake, solved an out of bounds error using gdb

---
+++ rgb/build/CMakeFiles/mandelbrot.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sam/Beckman-Mandelbrot/rgb/src/calc.cpp" "/home/sam/Beckman-Mandelbrot/rgb/build/CMakeFiles/mandelbrot.dir/src/calc.cpp.o"
+  "/home/sam/Beckman-Mandelbrot/rgb/src/class.cpp" "/home/sam/Beckman-Mandelbrot/rgb/build/CMakeFiles/mandelbrot.dir/src/class.cpp.o"
+  "/home/sam/Beckman-Mandelbrot/rgb/src/imag.cpp" "/home/sam/Beckman-Mandelbrot/rgb/build/CMakeFiles/mandelbrot.dir/src/imag.cpp.o"
+  "/home/sam/Beckman-Mandelbrot/rgb/src/primary.cpp" "/home/sam/Beckman-Mandelbrot/rgb/build/CMakeFiles/mandelbrot.dir/src/primary.cpp.o"
+  "/home/sam/Beckman-Mandelbrot/rgb/src/write.cpp" "/home/sam/Beckman-Mandelbrot/rgb/build/CMakeFiles/mandelbrot.dir/src/write.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../third_party"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
