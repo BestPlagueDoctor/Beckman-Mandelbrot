3656a4348ddf07902536467d5f5c046cd0a14257
BestPlagueDoctor <ksam1337@gmail.com>
2019-08-08 16:43:04 -0500

    Fixing Histogram Normalization

--- rgb/src/write.cpp
+++ rgb/src/write.cpp
@@ -1,11 +1,13 @@
 #include "proto.h"
+#include <string>
+#include <iostream>
 #include <vector>
 #include <cstdio>
 #include <cmath>
 
 
 void rescale_colors_global(std::vector<float> &zeroimg, std::vector<int> &img, int xres, int yres, int maxiter, int res) {
-  float maxdiv = 1.0 / maxiter;
+//  float maxdiv = 1.0 / maxiter;
   for (int i = 0; i < (xres*yres); i++) {
 //    zeroimg[i] = img[i] * maxdiv;
     //printf("%d, ", img[i]);
@@ -14,36 +16,22 @@ void rescale_colors_global(std::vector<float> &zeroimg, std::vector<int> &img, i
      // printf("%.2f, ", zeroimg[i]);
     //}
   }
-  float total = 0.0;
-  for (int i = 0; i < res; i++) {
-    total+=zeroimg[i];
-  }
-  printf("%f", total);
+  float total = res;
+ // printf("%f", total);
   int i;
-  auto rescaler = std::vector<int>(res+1);
-  for (i = 0; i < res; i+=14) {
-    rescaler.clear();
-    rescaler.resize(res+1);
-    //printf("%.2f, ", zeroimg[i]);
-    for (const auto& x: zeroimg) {
-      rescaler[x]++;
-    }
-    zeroimg[i] += rescaler[i] / total;
-    zeroimg[i+1] += rescaler[i+1] / total;
-    zeroimg[i+2] += rescaler[i+2] / total;
-    zeroimg[i+3] += rescaler[i+3] / total;
-    zeroimg[i+4] += rescaler[i+4] / total;
-    zeroimg[i+5] += rescaler[i+5] / total;
-    zeroimg[i+6] += rescaler[i+6] / total;
-    zeroimg[i+7] += rescaler[i+7] / total;
-    zeroimg[i+8] += rescaler[i+8] / total;
-    zeroimg[i+9] += rescaler[i+9] / total;
-    zeroimg[i+10] += rescaler[i+10] / total;
-    zeroimg[i+11] += rescaler[i+11] / total;
-    zeroimg[i+12] += rescaler[i+12] / total;
-    zeroimg[i+13] += rescaler[i+13] / total;
-    ///printf("%.2f ", zeroimg[i]);
-    printf("%d, ", i);
+  auto rescaler = std::vector<float>(res+1);
+  float totalinv = 1.0f/total;
+  rescaler.resize(res+1);
+  for (int j = 0; j < res; j++) {
+    rescaler[zeroimg[j]]++;
+  }
+  float sum = 0.0;
+  for (int j = 0; j < maxiter; j++) {
+    sum += rescaler[j];
+    rescaler[j] = sum * totalinv;
+  }
+  for (i = 0; i < res; i++) {
+    zeroimg[i] = rescaler[i];
   }
 }
 
@@ -92,12 +80,12 @@ void write_ppm(const char *filename, std::vector<float> &zeroimg, int xres, int 
   //ppm header
   fprintf(ofp, "P3\n");
   fprintf(ofp, "%d %d \n", xres, yres);
-  fprintf(ofp, "%d \n", 255);
+  fprintf(ofp, "%d \n", maxiter);
   int i; 
   for (i = 0; i < res; i++) {
-    int r = 0, g = 0, b = 0.0;
-    //r = zeroimg[i];
-    g = zeroimg[i];
+    int r = 0, g = 0, b = 0;
+    r = maxiter * zeroimg[i];
+    //g = zeroimg[i];
     //b = zeroimg[i];
     //r = 255-zeroimg[i];
     //g = 255-zeroimg[i];
